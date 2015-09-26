da92dc9439ae4a4e765e7e0f03f4cf09e0c96830
aptarmy <apt.enjoy@gmail.com>
2015-09-26 18:18:32 +0700

    add new 5 menues

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WordPress Tool Library</title>
+    <link rel="stylesheet" type="te.." href="<?php echo get_template_directory_uri() ../bootstrap.css">
+    <style>
+        * { margin:0; padding:0; text-align:center; box-sizing:border-box;}
+        .block { background: #fff; border-bottom: 1px solid #ddd; margin-top: 30px; }
+        article { height: 250px; line-height: 250px; }
+        a { text-decoration: none; color: #777; }
+        h2 { font-size: 32px; }
+    </style>
+</head>
+<body style="background-color: #f1f1f1;font-family:Arial; color:#888;">
+    <div class="container">
+        <h1 style="text-aligncenter; margin-top: 30px;">WordPress Theme Library</h1>
+        <div class="row">
+            <div class="col-md-4"><article class="block"><a href="<?php echo get_template_directory_uri() ?>/demo/skeleton-index.html"><h2>Menu</h2></a></article></div>
+            <div class="col-md-4"><article class="block"><a href="<?php echo get_template_directory_uri() ?>/demo/skeleton-index.html"><h2>Slider</h2></a></article></div>
+            <div class="col-md-4"><article class="block"><a href="<?php echo get_template_directory_uri() ?>/demo/skeleton-index.html"><h2>Posts</h2></a></article></div>
+            <div class="col-md-4"><article class="block"><a href="<?php echo get_template_directory_uri() ?>/demo/skeleton-index.html"><h2>Page Navigation</h2></a></article></div>
+            <div class="col-md-4"><article class="block"><a href="<?php echo get_template_directory_uri() ?>/demo/skeleton-post.html"><h2>Post</h2></a></article></div>
+            <div class="col-md-4"><article class="block"><a href="<?php echo get_template_directory_uri() ?>/demo/skeleton-post.html"><h2>Comment</h2></a></article></div>
+            <div class="col-md-4"><article class="block"><a href="<?php echo get_template_directory_uri() ?>/demo/skeleton-post.html"><h2>Widgets</h2></a></article></div>
+            <div class="col-md-4"><article class="block"><a href="<?php echo get_template_directory_uri() ?>/demo/skeleton-index.html"><h2>Loading</h2></a></article></div>
+        </div>
+    </div>
+</body>
+</html>
